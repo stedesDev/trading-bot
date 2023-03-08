fd33faa017087ddb2853291299e51101cb5bd53f
stephane.desjardins-qc <stephane.desjardins-ext@mcn.gouv.qc.ca>
2023-03-08 13:47:36 -0500

    Added the logger

--- bot.py
+++ bot.py
@@ -1,1 +1,51 @@
-print("Hello world")
+####################
+# define asset
+
+#####################
+# INITIAL CHECK
+# check position
+
+# check if tradable
+
+#####################
+# GENERAL TREND: until timout is reach (2 hours) and go back to general trend
+# load 30 mins candles
+
+# perform general trend analysis: checking if there is a trend up/down
+
+
+#####################
+# LOOP: until timout is reach (30 min) and go back to general trend
+# perform 5 mins trend analysis
+
+# perform instant trend analysis
+
+# perform RSI trend analysis
+
+# perform Stochastic trend analysis
+
+######################
+# SUMMIT ORDER
+# summit order
+
+# check position
+
+######################
+# ENTER POSITION: MODE LOOP
+# check take profit
+
+# check  stop loos
+
+# check crossing
+
+######################
+# GET OUT
+# check order
+
+# check position
+
+######################
+# WAIT
+# wait N mins
+
+# back to beggining
